27eb624d001f360b3678166246e41068b32d73b4
yeswon9874 <yeswon9874@gmail.com>
2023-10-23 14:55:26 +0900

    2023/10/23 axios 사용

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
     >
     <hr />
     <TodoSimpleForm @add-todo="addTodo" />
+    <div style="color: red">{{ error }}</div>
     
     <div v-if="!filteredTodos.length">
       There is nothing to display
@@ -25,6 +26,7 @@
 import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -34,9 +36,21 @@ export default {
   setup() {
     
     const todos = ref([]);
+    const error = ref('');
 
     const addTodo = (todo) => {
-      todos.value.push(todo);
+      // 데이터베이스 투두를 저장
+      error.value = '';
+      axios.post('http://localhost:3000/todos', {
+        subject: todo.subject,
+        completed: todo.completed,
+      }).then(res => {
+        todos.value.push(res.data);
+        console.log(res);
+      }).catch(err => {
+        console.log(err)
+        error.value = 'Something went wrong.';
+      });
     };
 
     const deleteButton = (index) => {
@@ -64,7 +78,8 @@ export default {
       deleteButton,
       toggleTodo,
       searchText,
-      filteredTodos
+      filteredTodos,
+      error
     };
   }
 }
